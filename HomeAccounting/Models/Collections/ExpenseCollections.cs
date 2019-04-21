4ee31b3015820b08344dd7c69f870ef5c3c160b7
Maksym Borukh <maksboruh@gmail.com>
2019-04-21 21:31:00 +0300

    add income and expense collections

---
+++ HomeAccounting/Models/Collections/ExpenseCollections.cs
@@ -0,0 +1,25 @@
+﻿using Models.Entities;
+using System.Collections.ObjectModel;
+
+namespace Models.Collections
+{
+    public class ExpenseCollections
+    {
+        private ObservableCollection<Expense> expenses;
+
+        public ExpenseCollections()
+        {
+            expenses = new ObservableCollection<Expense>();
+        }
+
+        public void Add(Expense expense)
+        {
+            expenses.Add(expense);
+        }
+
+        public ObservableCollection<Expense> GetExpenses()
+        {
+            return expenses;
+        }
+    }
+}
---
+++ HomeAccounting/Models/Collections/IncomeCollections.cs
@@ -0,0 +1,25 @@
+﻿using Models.Entities;
+using System.Collections.ObjectModel;
+
+namespace Models.Collections
+{
+    public class IncomeCollections
+    {
+        private ObservableCollection<Income> incomes;
+
+        public IncomeCollections()
+        {
+            incomes = new ObservableCollection<Income>();
+        }
+
+        public void Add(Income income)
+        {
+            incomes.Add(income);
+        }
+
+        public ObservableCollection<Income> GetExpenses()
+        {
+            return incomes;
+        }
+    }
+}
