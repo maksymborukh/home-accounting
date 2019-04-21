4c7b6f860d3eaf2d49be2d60f6c9dedb60fd98ad
Maksym Borukh <maksboruh@gmail.com>
2019-04-21 21:07:11 +0300

    add models and repositories

---
+++ HomeAccounting/Models/Entities/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class Expense
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double Amount { get; set; }
+        public double Percent { get; set; }
+        public string Date { get; set; }
+    }
+}
